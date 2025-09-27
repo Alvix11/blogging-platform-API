08b8c164e25751d09bccb8168db2f5d2b4c796bd
Alvix <alvix.wick@gmail.com>
2025-09-27 19:40:49 -0400

    Created the model for posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.CharField()
+    tags = ArrayField(
+        models.CharField(max_length=50),
+        blank=True,
+        default=list
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
