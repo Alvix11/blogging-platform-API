4642024df30877838f11880968a46566ba1004ca
Alvix <alvix.wick@gmail.com>
2025-09-28 01:19:23 -0400

    Added the URL to retrieve a specific post

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostCreateView
+from .views import PostListView, PostCreateView, PostGetView
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),
-    path('post/create/', PostCreateView.as_view(), name='post-create')
+    path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostGetView.as_view(), name='post-get'),
 ]
