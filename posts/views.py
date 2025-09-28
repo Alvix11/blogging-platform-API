1c972443933b2acad8142e4d55b8322bc74cd7f6
Alvix <alvix.wick@gmail.com>
2025-09-27 23:53:31 -0400

    Created a view to list posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+class PostListAPIView(APIView):
+    """
+    View to list posts using APIView
+    """
+    
+    def get(self, request):
+        """Handles GET requests (list all posts)"""
+        
+        # Obtain data from the database
+        posts = Post.objects.all()
+        
+        # Serialize data (convert to JSON)
+        serializer = PostSerializer(posts, many=True)
+        
+        return Response({
+            'success': True,
+            'count': len(posts),
+            'data': serializer.data
+        })
