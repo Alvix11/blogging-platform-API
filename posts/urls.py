704f8c5a01a9347cd5d26eff3192b7b9f07cba87
Alvix <alvix.wick@gmail.com>
2025-09-28 23:57:07 -0400

    Added the URL for to get filtered posts.

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostCreateView, PostGetView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostCreateView, PostGetView, PostUpdateView, PostDeleteView, PostFilterView
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),
     path('post/create/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostGetView.as_view(), name='post-get'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete')
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<str:word>/', PostFilterView.as_view(), name='post-filter'),
 ]
