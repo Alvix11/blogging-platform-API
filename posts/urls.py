0fe1f365082e2f36fe1c0c27cf11c9df4f4b8f00
Alvix <alvix.wick@gmail.com>
2025-09-28 21:10:52 -0400

    Added the URL for the update view.

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostCreateView, PostGetView
+from .views import PostListView, PostCreateView, PostGetView, PostUpdateView
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),
     path('post/create/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostGetView.as_view(), name='post-get'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
