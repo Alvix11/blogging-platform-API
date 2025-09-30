2d4b0a355c8ec98d9a3961b620c6d62453ef9566
Alvix <alvix.wick@gmail.com>
2025-09-29 21:43:20 -0400

    Added comments to make the code easier for other developers to understand.

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -21,12 +21,17 @@ class PostCreateViewTest(APITestCase):
         
         response = self.client.post(url, data, format='json')
         
+        # Verify that it returns 400
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        #Verify that it was created in the database and that the title matches
         self.assertEqual(Post.objects.count(), 1)
         self.assertEqual(Post.objects.first().title, "My last post")
         
     def test_create_post_invalid_data(self):
-        
+        """
+        Should fail when sending invalid data (e.g. missing title)
+        """
         
         url = reverse('post-create')
         
@@ -36,5 +41,7 @@ class PostCreateViewTest(APITestCase):
         
         response = self.client.post(url, data, format='json')
         
+        # Verify that it returns 400
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # It should not create anything
         self.assertEqual(Post.objects.count(), 0)
